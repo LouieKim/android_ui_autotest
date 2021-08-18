98f9343ef292a11e641f365596a9f8c089c4938b
Louie Kim <hyeok0724.kim@gmail.com>
2021-08-18 14:56:38 +0900

    amount test

--- macro_tap.py
+++ macro_tap.py
@@ -16,11 +16,40 @@ def connect():
 
 if __name__ == '__main__':
     device, client = connect()
+    
+    #이전 버튼
+    pst_back = '905 60'
 
+    #init 화면 한가운데 버튼
     pst_init = '512 315'
 
+    #충전타입 BType, CType 선택
     pst_chrgr_type = '724 359'
+
+    #금액 설정
+    pst_amount_chrgr = '143 355'
+
+    #시간 설정
+    pst_time_chrgr = '380 355'
+
+    #용량 설정
+    pst_vol_chrgr = '620 355'
+
+    #설정에서 1번째 버튼
+    pst_first_btn = '170 370'
+
+    #설정에서 3번째 버튼
+    pst_third_btn = '620 370'
+    
+    #설정에서 5번째 버튼
+    pst_fifth_btn = '400 470'
+
+    #설정 완료
+    pst_complete_set = '850 430'
+    
+    #가득 충전
     pst_full_chrgr = '851 355'
+
     pst_member = '285 363'
     pst_terminate_chrgr = '843 474'
     pst_terminate_chck = '701 395'
@@ -31,13 +60,31 @@ if __name__ == '__main__':
         print('Start Charger')
 
         device.shell(f'input tap {pst_init}')
-        time.sleep(3)
+        time.sleep(1)
 
         device.shell(f'input tap {pst_chrgr_type}')
-        time.sleep(3)
+        time.sleep(2)
 
-        device.shell(f'input tap {pst_full_chrgr}')
-        time.sleep(3)
+        #================================================
+        device.shell(f'input tap {pst_amount_chrgr}')
+        time.sleep(2)
+
+        device.shell(f'input tap {pst_first_btn}')
+        time.sleep(1)
+
+        device.shell(f'input tap {pst_third_btn}')
+        time.sleep(1)
+
+        device.shell(f'input tap {pst_fifth_btn}')
+        time.sleep(1)
+
+        device.shell(f'input tap {pst_complete_set}')
+        time.sleep(1)
+
+        #================================================
+        #device.shell(f'input tap {pst_full_chrgr}')
+        #time.sleep(3)
+        #================================================
 
         device.shell(f'input tap {pst_member}')
         time.sleep(15)
